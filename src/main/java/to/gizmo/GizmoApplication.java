89834f6a57a693bfaaf5e77762b189ba37992fff
Jan Malisz <jan.malisz@fingoweb.com>
2019-03-27 22:35:29 +0100

    Added generated stuff

---
+++ src/main/java/to/gizmo/GizmoApplication.java
@@ -0,0 +1,15 @@
+package to.gizmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GizmoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(GizmoApplication.class, args);
+    }
+
+}
