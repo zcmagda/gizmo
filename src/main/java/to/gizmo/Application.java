2b3fbea7026f97630633d3d27ba301c00bb20f85
Jan Malisz <jan.malisz@fingoweb.com>
2019-03-27 22:43:14 +0100

    Added greeting controller

--- src/main/java/to/gizmo/Application.java
+++ src/main/java/to/gizmo/Application.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GizmoApplication
+public class Application
 {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(GizmoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
