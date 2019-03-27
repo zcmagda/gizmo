e24582ddc00e39403ef779128df1502532ef30a1
Jan Malisz <jan.malisz@fingoweb.com>
2019-03-27 21:37:29 +0100

    Added spring initializer content

---
+++ src/main/java/pl/edu/agh/gizmo/GizmoApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.gizmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GizmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GizmoApplication.class, args);
+	}
+
+}
