683d4740d22f7861196b60c3dfaf2260ead7d33e
Jan Malisz <mistyfiky@gmail.com>
2019-05-22 01:06:36 +0200

    Added testLogin, testRegister and testRegisterProcess

--- src/test/java/to/gizmo/controllers/GizmoControllerTest.java
+++ src/test/java/to/gizmo/controllers/GizmoControllerTest.java
@@ -1,37 +1,87 @@
 package to.gizmo.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import to.gizmo.entities.User;
+import to.gizmo.repositories.UserRepository;
 
 @WebAppConfiguration
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest
-public class GizmoControllerTest {
-
+public class GizmoControllerTest
+{
     @Autowired
+    private WebApplicationContext context;
+
     private MockMvc mockMvc;
 
+    @Autowired
+    UserRepository userRepositoryMock;
+
+    @Before
+    public void setup()
+    {
+        this.mockMvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .apply(springSecurity())
+            .build();
+    }
+
     @Test
-    @WithMockUser(username="admin", roles={"USER","ADMIN"})
-    public void testIndex() throws Exception{
+    @WithMockUser(username = "admin")
+    public void testIndex() throws Exception
+    {
         mockMvc.perform(get("/"))
             .andExpect(status().isOk())
             .andExpect(view().name("index"))
             .andDo(print());
     }
 
+    @Test
+    public void testLogin() throws Exception
+    {
+        mockMvc.perform(get("/login"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("login"));
+    }
+
+    @Test
+    public void testRegister() throws Exception
+    {
+        mockMvc.perform(get("/register"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("register"));
+    }
+
+    @Test
+    public void testRegisterProcess() throws Exception
+    {
+        User user = new User();
+        user.setUsername("user");
+        user.setPassword("password");
+
+        mockMvc.perform(
+            post("/register")
+                .param("username", user.getUsername())
+                .param("password", user.getPassword()))
+            .andExpect(redirectedUrl("/login"));
+    }
 }
