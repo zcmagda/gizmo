c559503b09c39fa85c14c03f94bfdca69bf09bb3
zcmagda <zc.magda@gmail.com>
2019-05-24 18:50:50 +0200

    Code formating

--- src/test/java/to/gizmo/controllers/BoardControllerTest.java
+++ src/test/java/to/gizmo/controllers/BoardControllerTest.java
@@ -1,10 +1,7 @@
 package to.gizmo.controllers;
 
-import static org.hamcrest.Matchers.hasValue;
-import static org.springframework.data.repository.util.ClassUtils.hasProperty;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
 
@@ -13,21 +10,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.WebApplicationContext;
-
-import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
-import to.gizmo.entities.Board;
-import to.gizmo.entities.Workspace;
-import to.gizmo.repositories.BoardRepository;
-import to.gizmo.repositories.WorkspaceRepository;
-import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @RequestMapping("/board")
@@ -40,22 +28,15 @@ public class BoardControllerTest {
     private MockMvc mockMvc;
 
     @Before
-    public void setup()
-    {
+    public void setup() {
         this.mockMvc = MockMvcBuilders
             .webAppContextSetup(context)
             .apply(springSecurity())
             .build();
     }
 
-    @Autowired
-    private BoardRepository boardRepository;
-
-    @Autowired
-    private WorkspaceRepository workspaceRepository;
-
     @Test
-    @WithMockUser(username="admin")
+    @WithMockUser(username = "admin")
     public void testBoardCreate() throws Exception {
         mockMvc.perform(
             get("/board/create/{workspaceId}", 1)
@@ -66,7 +47,7 @@ public class BoardControllerTest {
 
 
     @Test
-    @WithMockUser(username="admin")
+    @WithMockUser(username = "admin")
     public void testBoardCreateProcess() throws Exception {
         mockMvc.perform(
             post("/board/create/{workspaceId}", 1)
@@ -77,29 +58,28 @@ public class BoardControllerTest {
     }
 
     @Test
-    @WithMockUser(username="admin")
+    @WithMockUser(username = "admin")
     public void testBoardUpdate() throws Exception {
         mockMvc.perform(
-            get("/board/update/{id}",1)
+            get("/board/update/{id}", 1)
         )
             .andExpect(status().isOk())
             .andExpect(view().name("board/update"));
     }
 
     @Test
-    @WithMockUser(username="admin")
+    @WithMockUser(username = "admin")
     public void testBoardUpdateProcess() throws Exception {
         mockMvc.perform(
-            post("/board/update/{id}",1)
+            post("/board/update/{id}", 1)
                 .param("title", "title")
         )
             .andExpect(redirectedUrl("/workspace/read/1"));
     }
 
 
-
     @Test
-    @WithMockUser(username="admin")
+    @WithMockUser(username = "admin")
     public void testBoardDelete() throws Exception {
         mockMvc.perform(post("/board/delete/{id}", 1))
             .andExpect(redirectedUrl("/workspace/read/1"));
